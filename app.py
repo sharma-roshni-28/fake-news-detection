d66ca739f9b198c67a4424872865758cc47b12b9
unknown <sharmarajvivek82@gmail.com>
2026-07-02 21:36:14 -0700

    Updated app

--- app.py
+++ app.py
@@ -1,12 +1,52 @@
 import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
 
-st.title("Fake News Detection System")
+# Load datasets
+fake = pd.read_csv("Fake.csv")
+true = pd.read_csv("True.csv")
+
+# Add labels
+fake["label"] = "Fake"
+true["label"] = "Real"
+
+# Combine datasets
+data = pd.concat([fake, true])
+
+# Use news text
+X = data["text"]
+y = data["label"]
+
+# Convert text into vectors
+vectorizer = TfidfVectorizer(stop_words='english')
+
+X_vector = vectorizer.fit_transform(X)
+
+# Train model
+X_train, X_test, y_train, y_test = train_test_split(
+    X_vector, y, test_size=0.2, random_state=42
+)
+
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+# Streamlit UI
+st.title("📰 Fake News Detection System")
 
 news = st.text_area("Enter News Here")
 
 if st.button("Check News"):
 
-    if "shocking" in news.lower():
-        st.error("Fake News Detected")
+    if news.strip() == "":
+        st.warning("Please enter news")
     else:
-        st.success("Real News")
+        news_vector = vectorizer.transform([news])
+
+        prediction = model.predict(news_vector)
+
+        if prediction[0] == "Fake":
+            st.error("⚠ Fake News Detected")
+        else:
+            st.success("✅ Real News")
